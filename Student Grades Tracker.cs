5afb036c17df5997b038cd1041fc706a9bfc664a
Mazen Elbakrey <mazenelbakrey@gmail.com>
2026-07-31 08:18:18 +0300

    Add files via upload

---
+++ Student Grades Tracker.cs
@@ -0,0 +1,63 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+static void Calculate_avgaandlevel(int[]grades,ref double avg,ref string level)
+{
+    double sum = 0;
+    for (int i = 0; i < grades.Length; i++)
+    {
+        sum += grades[i];
+    }
+    avg = sum / grades.Length;
+
+
+    if (avg >= 0.0 && avg < 20)
+    {
+        level = "Freshman";
+    }
+    else if (avg >= 20 && avg < 40)
+    {
+        level = "Sophomore";
+    }
+    else if (avg >= 40 && avg < 60)
+    {
+        level = "Junior";
+    }
+    else if (avg >= 60 && avg <= 100)
+    {
+        level = "Senior";
+    }
+}
+
+static void display(string name,int[] grades,  double avg, string level)
+{
+    Console.WriteLine("------------------------");
+    Console.WriteLine($"Student Name : {name}");
+
+    Console.Write($"Grades : ");
+    for (int i = 0; i < grades.Length; i++)
+    {
+        Console.Write($"{grades[i]} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Average : {avg}");
+
+    Console.WriteLine($"level : {level}");
+    Console.WriteLine("------------------------");
+}
+
+    double avg = 0;
+    Console.WriteLine("Enter your name : ");
+    string name = Console.ReadLine();
+
+    int[] grades = new int[5];
+
+    for (int i = 0; i < grades.Length; i++)
+    {
+        Console.WriteLine($"Enter your grade {i + 1} : ");
+        grades[i] = int.Parse(Console.ReadLine());
+    }
+    String level = " ";
+
+    Calculate_avgaandlevel(grades, ref avg,ref level);
+    display(name, grades,  avg, level);
---
+++ Tic-Tac-Toe.cs
@@ -0,0 +1,156 @@
+﻿static void DrawBoard(string[,] board)
+{
+    Console.WriteLine(" ------------------- ");
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            Console.Write($" | {board[i, j]} ");
+        }
+        Console.WriteLine("|");
+        Console.WriteLine(" ------------------- ");
+    }
+}
+
+static string GetValidInput(string[,] board)
+{
+    while (true)
+    {
+        Console.Write("Choose position (1-9): ");
+        string input = Console.ReadLine();
+
+        bool found = false;
+
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (board[i, j] == input)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+                break;
+        }
+
+        if (found)
+        {
+            return input;
+        }
+        else
+        {
+            Console.WriteLine("Invalid position or position already taken!");
+        }
+    }
+}
+
+static void Edit(ref string[,] board, string ch, string num)
+{
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            if (board[i, j] == num)
+            {
+                board[i, j] = ch;
+                return;
+            }
+        }
+    }
+}
+
+static bool CheckWinner(string[,] board)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+        {  return true; }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+        { return true; }
+    }
+
+    if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+        { return true; }
+
+    if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+    { return true; }
+
+    else
+    {
+        return false;
+    }
+}
+
+static bool IsDraw(string[,] board)
+{
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            if (board[i, j] != "X" && board[i, j] != "O")
+                return false;
+        }
+    }
+    return true;
+}
+
+string[,] board =
+{
+    { "1", "2", "3" },
+    { "4", "5", "6" },
+    { "7", "8", "9" }
+};
+
+while (true)
+{
+    DrawBoard(board);
+
+    Console.WriteLine("Player 1 Turn (X)");
+    string p1 = GetValidInput(board);
+
+    Edit(ref board, "X", p1);
+
+    if (CheckWinner(board))
+    {
+        DrawBoard(board);
+        Console.WriteLine(" Player 1 Wins!");
+        break;
+    }
+
+    if (IsDraw(board))
+    {
+        DrawBoard(board);
+        Console.WriteLine(" Draw!");
+        break;
+    }
+
+    DrawBoard(board);
+
+    Console.WriteLine("Player 2 Turn (O)");
+    string p2 = GetValidInput(board);
+
+    Edit(ref board, "O", p2);
+
+    if (CheckWinner(board))
+    {
+        DrawBoard(board);
+        Console.WriteLine(" Player 2 Wins!");
+        break;
+    }
+
+    if (IsDraw(board))
+    {
+        DrawBoard(board);
+        Console.WriteLine(" Draw!");
+        break;
+    }
+}
+
+Console.WriteLine("Game Over!");
